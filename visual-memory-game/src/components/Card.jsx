622315d6d7c06974652dc934d5200bcdc3b3eb15
Misha2007 <mishadrogovoz@gmail.com>
2025-01-12 14:05:33 +0200

    Updating home page and changing game logics

--- visual-memory-game/src/components/Card.jsx
+++ visual-memory-game/src/components/Card.jsx
@@ -1,6 +1,12 @@
 const Card = (props) => {
   return (
-    <div className={`block ${props.active}`} onClick={props.onClick}></div>
+    <div
+      className={`block ${props.active} ${
+        props.clicked ? "clicked-color" : ""
+      }`}
+      onClick={!props.disabled ? props.onClick : undefined}
+      style={{ pointerEvents: props.disabled ? "none" : "auto" }}
+    ></div>
   );
 };
 
--- visual-memory-game/src/components/Home.jsx
+++ visual-memory-game/src/components/Home.jsx
@@ -1,27 +1,61 @@
-import React from "react";
+// Home.jsx
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import "./HomeStyle.css";
 
 const Home = () => {
+  const [leaderboardData, setLeaderboardData] = useState([]);
+
+  const fetchLeaderboardData = async () => {
+    try {
+      const response = await fetch("http://localhost:3001/leaderboard");
+      const data = await response.json();
+      setLeaderboardData(data);
+    } catch (error) {
+      console.error("Error fetching leaderboard data:", error);
+    }
+  };
+
+  useEffect(() => {
+    fetchLeaderboardData();
+  }, []);
+
   return (
     <div className="all">
       <div className="home">
+        {/* <h1 className="vmg vmg-3d">Visual Memory Game</h1>
+        <div className="btnContainer">
+          <Link to="/game">
+            <button className="btn">Start Game</button>
+          </Link>
+        </div> */}
         <div className="innerBackground">
           <span className="material-symbols-outlined icon"></span>
-          <h1>Visual Memory Game</h1>
-          <p className="pps">Memorize the Squares</p>
+          <h1 className="vmg">FlashMind</h1>
+          <u>
+            <p className="pps">Tap on the flashed squares</p>
+          </u>
+
+          <h2>How to Play</h2>
+          <p>1. A sequence of blocks will light up.</p>
+          <p>2. Click the blocks in the same order.</p>
+          <p>3. Successfully complete levels to increase difficulty.</p>
+          <div className="btnContainer">
+            <Link to="/game">
+              <button className="btn">Start Game</button>
+            </Link>
+          </div>
+          {/* <div id="leaderboard">
+            <h3>Leaderboard</h3>
+            <ul>
+              {leaderboardData.map((entry, index) => (
+                <li key={index}>
+                  {entry.username} - Level: {entry.level}, Time: {entry.timer}s
+                </li>
+              ))}
+            </ul>
+          </div> */}
         </div>
-      </div>
-      <div className="tutorial">
-        <h2>How to Play</h2>
-        <p>1. A sequence of blocks will light up.</p>
-        <p>2. Click the blocks in the same order.</p>
-        <p>3. Successfully complete levels to increase difficulty.</p>
-      </div>
-      <div className="btnContainer">
-        <Link to="/game">
-          <button className="btn">Start Game</button>
-        </Link>
       </div>
     </div>
   );
